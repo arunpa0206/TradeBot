ae5d58d6d749da922c1c74f2a255cd286493858b
ashutosh0709 <61544363+ashutosh0709@users.noreply.github.com>
2021-08-19 18:24:49 +0530

    Delete kafka.py

--- kafka.py
+++
@@ -1,5 +0,0 @@
-from json import dumps
-from kafka import KafkaProducer
-
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=lambda x: dumps(x).encode('utf-8'),api_version=(2, 0, 2)) #kafka starter
-
